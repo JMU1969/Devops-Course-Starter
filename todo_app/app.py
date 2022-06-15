429f45c9391beae0c5b56bcb5474e2eb63e8ad65
James Murray <jmurray69@gmail.com>
2022-06-15 15:29:12 +0100

    todo

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,6 @@
 from flask import Flask
-from flask import render_template
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -10,5 +8,4 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    items = get_items()
-    return render_template("index.html", items = items)
+    return 'Hello World!'
